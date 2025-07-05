6891756c1ca589fd2a69d626f02d9d196d79f51b
sandeyez <sander@jojoschool.nl>
2025-07-05 22:54:53 +0200

    fix(MovieCast): remove unused imports

--- app/components/movie/movie-cast.tsx
+++ app/components/movie/movie-cast.tsx
@@ -1,11 +1,6 @@
+import { cn } from "@/lib/tailwind";
 import { Cast } from "tmdb-ts";
 import { CastItem } from "./cast-item";
-import { useLayoutEffect, useRef, useState } from "react";
-import { motion } from "framer-motion";
-import { Button } from "../ui/button";
-import { ChevronDownIcon } from "lucide-react";
-import { cn } from "@/lib/tailwind";
-import { Link, useParams } from "@tanstack/react-router";
 
 type MovieCastProps = {
   cast: Cast[];
@@ -19,7 +14,7 @@ export function MovieCast({ cast, showAll }: MovieCastProps) {
         "overflow-hidden w-full @container/cast transition-[height] duration-200 ease-in-out"
       )}
     >
-      <ul className="grid grid-cols-2 @sm/cast:grid-cols-3 @lg/cast:grid-cols-4 @2xl/cast:grid-cols-5 @3xl/cast:grid-cols-6 gap-x-6 gap-y-8 mt-3 *:empty:hidden">
+      <ul className="grid grid-cols-2 @sm/cast:grid-cols-3 @lg/cast:grid-cols-4 @2xl/cast:grid-cols-5 @3xl/cast:grid-cols-6 gap-x-6 gap-y-8 *:empty:hidden">
         {showAll ? (
           cast.map((cast) => (
             <li key={cast.id}>
