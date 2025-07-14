a0f5acade4eb5afebe10bdcf04e183c2762f8c07
sandeyez <sander@jojoschool.nl>
2025-07-14 21:32:50 +0200

    style: dynamic grid column width calculation

--- src/routes/_app/search.tsx
+++ src/routes/_app/search.tsx
@@ -62,7 +62,7 @@ export const Route = createFileRoute("/_app/search")({
   },
 });
 
-const gridContainerClassName = tw`mt-4 grid xs:grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-x-4 gap-y-6`;
+const gridContainerClassName = tw`mt-4 grid grid-cols-[repeat(auto-fill,minmax(10rem,1fr))] gap-x-4 gap-y-6`;
 
 function RouteComponent() {
   const { q } = Route.useSearch();
