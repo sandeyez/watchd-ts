b66438e9702948edf92c0ddec4c877731e42df14
sandeyez <sander@jojoschool.nl>
2025-10-07 21:34:41 +0200

    feat(schema): add movie_reviews table and update schema with new types

--- app/db/schema.ts
+++ app/db/schema.ts
@@ -1,4 +1,11 @@
-import { pgEnum, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import {
+  integer,
+  pgEnum,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+} from "drizzle-orm/pg-core";
 
 export const userStatusEnum = pgEnum("status", ["ACTIVE", "DELETED"]);
 
@@ -10,3 +17,17 @@ export const users = pgTable("users_table", {
 
 export type InsertUser = typeof users.$inferInsert;
 export type SelectUser = typeof users.$inferSelect;
+
+export const movieReviews = pgTable("movie_reviews", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  userId: text("user_id")
+    .references(() => users.id)
+    .notNull(),
+  movieId: integer("movie_id").notNull(),
+  rating: integer("rating").notNull(),
+  review: text("review").notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+});
+
+export type InsertMovieReview = typeof movieReviews.$inferInsert;
+export type SelectMovieReview = typeof movieReviews.$inferSelect;
