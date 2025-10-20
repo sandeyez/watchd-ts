ae2a14708d01ef52b8c7a13a7badeb1a64d70fe4
sandeyez <sander@jojoschool.nl>
2025-10-20 23:19:21 +0200

    feat(db): integrate schema into drizzle initialization for enhanced database management

--- app/db/index.server.ts
+++ app/db/index.server.ts
@@ -3,8 +3,10 @@ import { neon, neonConfig } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import ws from "ws";
 
+import * as schema from "./schemas";
+
 neonConfig.webSocketConstructor = ws;
 
 const sql = neon(process.env.DATABASE_URL!);
 
-export const db = drizzle({ client: sql });
+export const db = drizzle({ client: sql, schema });
