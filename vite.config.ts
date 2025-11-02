5990b26562b87a26835764689d6b0d2143f1506e
sandeyez <sander@jojoschool.nl>
2025-11-02 16:23:40 +0100

    cicd: add nitro vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [tsConfigPaths(), tanstackStart(), tailwindcss()],
+  plugins: [tsConfigPaths(), tanstackStart(), nitro(), tailwindcss()],
 });
